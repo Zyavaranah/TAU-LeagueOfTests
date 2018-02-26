0466fb4b0044539aee912a16f380b72f67afef08
Zyavaranah <32269336+Zyavaranah@users.noreply.github.com>
2018-02-26 14:15:41 +0100

    Update and rename AppTest.java to ChampionNameTest.java

---
+++ league-of-tests/src/test/java/org/leagueoftests/app/ChampionNameTest.java
@@ -0,0 +1,10 @@
+package org.leagueoftests.app;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class ChampionNameTest extends TestCase
+{
+    
+}
