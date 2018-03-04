9e7c4a7b582799117e9a4182b367e867ca3f059b
Zyavaranah <32269336+Zyavaranah@users.noreply.github.com>
2018-03-04 07:40:42 +0100

    Update ChampionNameTest.java

--- league-of-tests/src/test/java/org/leagueoftests/app/ChampionNameTest.java
+++ league-of-tests/src/test/java/org/leagueoftests/app/ChampionNameTest.java
@@ -1,11 +1,12 @@
 package org.leagueoftests.app;
- 
+
+import org.junit.Test;
 import static org.junit.Assert.assertNotNull;
 
 public class CharacterNameTest {
     @Test
     public void createTest() {
         Champions champion = new Champion();
-        assertNotNull(champion.championName);
+        assertNotNull(champion);
     }
 }
