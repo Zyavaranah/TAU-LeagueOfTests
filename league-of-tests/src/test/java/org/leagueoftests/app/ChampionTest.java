4fc0c68ea918065648e578d60186f9395c6ee1d0
Zyavaranah <32269336+Zyavaranah@users.noreply.github.com>
2018-03-04 21:19:42 +0100

    Update ChampionTest.java

--- league-of-tests/src/test/java/org/leagueoftests/app/ChampionTest.java
+++ league-of-tests/src/test/java/org/leagueoftests/app/ChampionTest.java
@@ -4,9 +4,11 @@ import org.junit.Test;
 import static org.junit.Assert.assertNotNull;
 
 public class ChampionTest {
+    
     @Test
     public void createTest() {
         Champions champion = new Champions();
         assertNotNull(champion);
     }
+    
 }
